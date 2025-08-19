5e8b2f806aa3884c04cd05ea1d386acf340d71e5
Vyom <vyomj.home@gmail.com>
2025-08-18 22:48:08 -0700

    v0.15(Added stockfish eval to new dict type, linked with web)

--- PositionGuessr.py
+++ PositionGuessr.py
@@ -1,40 +1,69 @@
-import warnings
-warnings.filterwarnings("ignore", category=UserWarning, module="urllib3")
+
+## Welcome to PositionGuessr, a chess position guessing game, where you guess whether the position is winning for white, losing, or equal
+## This application will be a successful project and will sell to lichess, chess.com, or any other chess platform, making millions. 
+
 import chess
 import chess.engine
 import chess.svg
 import berserk
 import pandas as pd
 from flask import Flask, render_template, send_file
+import csv
+with open("positions.csv", mode="r", newline="", encoding="utf-8") as f:
+    # Create a DictReader object
+    # DictReader automatically maps each row into a dictionary
+    # using the first line of the CSV (the header) as keys
+    reader = csv.DictReader(f)
+
+    # Convert the reader into a list of dictionaries
+    data = []
+    for row in reader:
+        data.append(row)
+
+filtered_data = []
+
 ##import stockfish
-data = pd.read_csv('positions.csv')
-df = pd.DataFrame(data)
+
 app = Flask(__name__)
 board = chess.Board()
 
-## Welcome to PositionGuessr, a chess position guessing game, where you guess whether the position is winning for white, losing, or equal
-## This application will be a successful project and will sell to lichess, chess.com, or any other chess platform
+## Initialize the Stockfish engine
+engine = chess.engine.SimpleEngine.popen_uci("stockfish")
+
 
 # This function evalutes the FEN position using Stockfish and returns the score
 def getEval(position):
     board = chess.Board(position)
     
     #analyse the position using Stockfish
-    engine = chess.engine.SimpleEngine.popen_uci("stockfish")
     result = engine.analyse(board, chess.engine.Limit(depth=20))
     score = result["score"].pov(chess.WHITE)
     
     return score, board
-
-
+## This function loads positions from dataset and returns the SVG text, eval, and board object. 
+def loadPositions():
+    for row in range(len(data)):
+        eval, board = getEval(data[row]['FEN'])
+        svg_data = chess.svg.board(board=board)
+        filtered_data.append({
+            'SVG':svg_data,
+            'Eval':eval.score()
+        })
+    print("Filtration DONE!!!!")
+    
+    ## Quit the engine to maximize performance
+    engine.quit()
+    return filtered_data 
+    
 ## This line of code is used to set the route for the Flask application, telling it to associating / path with the index function
 @app.route("/")
 def index():
-    eval, board = getEval(df['FEN'][0])
-    svg_data = chess.svg.board(board=board)
+    filtered_data = loadPositions()
+    print(filtered_data[5]['Eval'])
     
     
-    return render_template('index.html', svg_board = svg_data, eval = eval.score())
+    
+    return render_template('index.html', filtered_data = filtered_data)
     
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
--- static/main.js
+++ static/main.js
@@ -1,10 +1,65 @@
 var result;
+var correct_result;
 var answered = false;
+// Firebase Configuration Code : 
+// Import the functions you need from the SDKs you need
+import { getDatabase, ref, set, onValue, get, update } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDtGbU8BN06Y_GNDmhV1FJFRhTvD603DN0",
+    authDomain: "positionguessr.firebaseapp.com",
+    databaseURL: "https://positionguessr-default-rtdb.firebaseio.com",
+    projectId: "positionguessr",
+    storageBucket: "positionguessr.firebasestorage.app",
+    messagingSenderId: "954415790631",
+    appId: "1:954415790631:web:0a5381589df51fc3abec02",
+    measurementId: "G-M63L8MVR6Z"
+  };
+
+var positions = [
+    {"FEN": 'r1r3k1/5ppp/nR6/3P4/8/P4N2/P4PPP/R5K1 b - - 0 29', 'Evaluation': 284}
+]
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+var db = getDatabase(app)
+
+
+
+
+
+
 window.onload = function() {
     correct_result = findResult();
     console.log(correct_result);
     
-};   
+}; 
+
+// THis function is called when the user clicks their guess. It checks if the user is correct or not, and displays the result.
+function sendAnswer(guess) {
+    if(answered){
+        return;
+    }
+    else if(guess === correct_result) {
+        document.getElementById("result").innerHTML = "Correct!"
+        document.getElementById("result").style.color = "green";
+        answered = true;
+    }
+    else {
+        document.getElementById("result").innerHTML = "Incorrect! The correct answer is " + correct_result + ".";
+        document.getElementById("result").style.color = "red";
+        answered = true;
+    }
+};
+window.sendAnswer = sendAnswer;
+    
 // This function finds the desired result based on the evaluation score. This result is then compared to the user's guess.
 function findResult(){
     
@@ -21,21 +76,8 @@ function findResult(){
 
     return result;
     
-}
-// Function to handle user guess and display result
-function sendAnswer(guess) {
-    if(answered){
-        return;
-    }
-    else if(guess === correct_result) {
-        document.getElementById("result").innerHTML = "Correct!"
-        document.getElementById("result").style.color = "green";
-        answered = true;
-    }
-    else {
-        document.getElementById("result").innerHTML = "Incorrect! The correct answer is that " + correct_result + " is winning.";
-        document.getElementById("result").style.color = "red";
-        answered = true;
-    }
-}
-                  
+};
+
+
+
+              
